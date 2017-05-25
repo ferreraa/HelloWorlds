fdbbe25cf86ab38081e61515aa7ccc3f26def0e3
ferreraa <alexandre78ferrera@gmail.com>
2017-05-25 16:38:54 -0300

    processbuilderExample is usable

--- src/helloworld/ProcessBuilderExample.java
+++ src/helloworld/ProcessBuilderExample.java
@@ -18,21 +18,28 @@ public class ProcessBuilderExample {
 	public static void main(String[] args) throws InterruptedException,
 			IOException {
 		String pathToScript = "/home/ferreraalexandre/Desktop/data-integration/";  //This must be an absolute path => no '~' allowed
+		
+//		execute("/bin/bash", pathToScript+"pan.sh", "/file", "tutorial/Hello.ktr", "/norep");
+		
+		execute("/bin/bash", pathToScript+"kitchen.sh", "./kitchen.sh", "/file", "/home/ferreraalexandre/Desktop/data-integration/tutorial/Hello.kjb", "list2.csv", "/norep");
+		
+		
+	}
 
-		
-		ProcessBuilder pb = new ProcessBuilder("/bin/bash", pathToScript+"pan.sh", "/file", "tutorial/Hello.ktr", "/norep");
+	/**
+	 * execute the given command
+	 * @param command format must be: "bin/bash" (for UNIX), path+command, arg1, arg2, etc...
+	 */
+	public static void execute(String ...command) throws InterruptedException, IOException{
+		ProcessBuilder pb = new ProcessBuilder(command);
 		//Thank you : https://examples.javacodegeeks.com/core-java/lang/processbuilder/java-lang-processbuilder-example/
-		
-		
-		System.out.println("Run echo command");
+
+		System.out.println("Run command: " + command.toString());
 		Process process = pb.start();
 		int errCode = process.waitFor();
 		System.out.println("command executed, any errors? " + (errCode == 0 ? "No" : "Yes: " + errCode));
-		System.out.println("Output:\n" + output(process.getInputStream()));	
+		System.out.println("Output:\n" + output(process.getInputStream()));			
 	}
-
-	
-	
 	
 	private static String output(InputStream inputStream) throws IOException {
 		StringBuilder sb = new StringBuilder();
